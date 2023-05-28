775a1287d48ea7892bc3c6d30dea970a6e42389c
marielaure1 <edjour.marielaure@gmail.com>
2023-05-28 22:29:08 +0200

    ajout methode me()

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { request, register, login, logout} from '../controllers/AuthController.js'
+import { request, register, login, logout, me} from '../controllers/AuthController.js'
 import { authentification } from "../middlewares/AuthMiddleware.js"
 
 
@@ -7,5 +7,6 @@ let authRoutes = express.Router()
 authRoutes.route('/register').post(request, register)  // S'inscrire
 authRoutes.route('/login').post(login)  // Se connecter
 authRoutes.route('/logout').post(authentification, logout)  // Se déconnecter
+authRoutes.route('/me').post(authentification, me)  // Récuperer les données de la personne connecter
 
 export default authRoutes
